cac33726a76807abd5dc75057f1a8e28988a1403
greatnanu <workforitshubham@gmail.com>
2018-08-11 09:28:48 +0530

    Initial Commit

---
+++ app/src/main/java/in/co/itshubham/launchscreen/MainActivity.java
@@ -0,0 +1,15 @@
+package in.co.itshubham.launchscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.SplashTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
